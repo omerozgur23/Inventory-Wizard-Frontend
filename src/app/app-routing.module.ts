f9942ce9d80bc41aca48d525fbba311decc90904
Şevval Yılmaz <yilmaz.sevvall99@gmail.com>
2024-04-06 03:33:02 +0300

    bootstrapModal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,9 +10,10 @@ import { ROLE_ADMIN, ROLE_EMPLOYEE } from './shared/model/constants';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path:'menu', component: MenuComponent, //  canActivate: [loginGuard], 
+  { path:'menu', component: MenuComponent, 
+  //  canActivate: [loginGuard], 
     children: [
-      { path: 'product-list', loadChildren: () => import('./modules/product/product.module').then(m => m.ProductModule) },
+      { path: 'product-list', loadChildren: () => import('./modules/product/product.module').then(m => m.ProductModule)},
       { path: 'employee-list', loadChildren: () => import('./modules/employee/employee.module').then(m => m.EmployeeModule)},
       { path: 'shelf-list', loadChildren: () => import('./modules/warehouse/warehouse.module').then(m => m.WarehouseModule)},
       { path: 'category-list', loadChildren: () => import('./modules/category/category.module').then(m => m.CategoryModule)},
